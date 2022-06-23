d7b228d6aacb45dfcf0dcde8645047594e7952af
Denki05 <denki.kiki05@gmail.com>
2022-06-23 11:03:38 +0700

    rev

--- resources/views/superuser/asset/css.blade.php
+++ resources/views/superuser/asset/css.blade.php
@@ -1,7 +1,11 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 <link rel="stylesheet" id="css-main" href="{{ asset('superuser_assets/css/codebase.min.css') }}">
 @if(setting('website.color_themes') != 'default')
 <link rel="stylesheet" href="{{ asset(setting('website.color_themes')) }}" id="css-theme" >
 @endif
 <link rel="stylesheet" href="{{ asset('superuser_assets/css/codebase.custom.css') }}">
 <link rel="stylesheet" href="{{ asset('superuser_assets/summernote/summernote.css') }}">
+<link rel="stylesheet" href="{{ asset('superuser_assets/css/login.css') }}">
+
--- resources/views/superuser/auth.blade.php
+++ resources/views/superuser/auth.blade.php
@@ -10,77 +10,30 @@
     @stack('plugin-styles')
   </head>
   <body>
-    <div id="page-container" class="main-content-boxed">
-      <header id="page-header">
-        <div id="page-header-loader" class="overlay-header bg-primary">
-          <div class="content-header content-header-fullrow text-center">
-            <div class="content-header-item">
-              <i class="fa fa-sun-o fa-spin text-white"></i>
-            </div>
-          </div>
-        </div>
-      </header>
-      <main id="main-container">
-        <div class="bg-body-dark">
-          <div class="row mx-0 justify-content-center">
-            <div class="hero-static col-lg-6 col-xl-4">
-              <div class="content content-full overflow-hidden">
-                {{-- <div class="py-30 text-center">
-                  <h1 class="h4 font-w700 mt-30 mb-10">Welcome to Your Dashboard</h1>
-                  <h2 class="h5 font-w400 text-muted mb-0">It’s a great day today!</h2>
-                </div> --}}
-                <form class="ajax js-validation-signin pt-150" data-action="{{ route('auth.superuser.login') }}" data-type="POST">
-                  <div class="block block-themed block-rounded block-shadow">
-                    <div class="block-header bg-gd-primary">
-                      <h3 class="block-title">Sign In</h3>
-                    </div>
-                    <div class="block-content">
-                      <div class="form-group">
-                        <label>Username / Email</label>
-                        <input type="text" class="form-control" name="account_name">
+                <form class="ajax js-validation-signin" data-action="{{ route('auth.superuser.login') }}" data-type="POST">
+                  <div class="wrapper">
+                      <div class="logo">
+                          <img src="https://www.premiumparfum.com/images/logo%20ppi%202016%20-%20variant-02-u26873_2x.png" alt="">
                       </div>
-                      <div class="form-group">
-                        <label>Password</label>
-                        <div class="input-group">
-                          <input type="password" class="form-control" name="password">
-                          <div class="input-group-append">
-                            <button type="button" class="input-group-text" id="toggle_password" data-toggle="false">
-                              <i class="fa fa-eye"></i>
-                            </button>
+                      <div class="text-center mt-4 name">
+                          
+                      </div>
+                      <form class="p-3 mt-3">
+                          <div class="form-field d-flex align-items-center">
+                              <span class="far fa-user"></span>
+                              <input type="text" name="account_name" id="account_name" placeholder="Username / Email">
                           </div>
-                        </div>
-                      </div>
-                      <div class="form-group row mb-0">
-                        <div class="col-6 d-sm-flex align-items-center push">
-                          <div class="mr-auto ml-0 mb-0">
-                            <label class="css-control css-control-secondary css-checkbox">
-                              <input type="checkbox" class="css-control-input" name="remember">
-                              <span class="css-control-indicator"></span> Remember me
-                            </label>
+                          <div class="form-field d-flex align-items-center">
+                              <span class="fas fa-key"></span>
+                              <input type="password" name="password" id="password" placeholder="Password">
                           </div>
-                        </div>
-                        <div class="col-6 text-right push">
-                          <button type="submit" class="btn btn-outline-secondary">
-                            <i class="si si-login mr-10"></i> Sign In
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    {{-- <div class="block-content bg-body-light">
-                      <div class="form-group text-center">
-                        <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="op_auth_reminder3.html">
-                          <i class="fa fa-warning mr-5"></i> Forgot Password
-                        </a>
-                      </div>
-                    </div> --}}
+                          <button class="btn mt-3">Login</button>
+                      </form>
+                      {{-- <div class="text-center fs-6">
+                          <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+                      </div>--}}
                   </div>
                 </form>
-                <div id="alert-block"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
     </div>
     @include('superuser.asset.js')
     @include('superuser.asset.plugin.notify')
