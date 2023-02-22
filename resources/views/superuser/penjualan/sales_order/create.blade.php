a0b4b37f5ae506fdad566c99a095bd8f5d735b76
Denki05 <denki.kiki05@gmail.com>
2023-02-22 13:17:38 +0700

    rev

--- resources/views/superuser/penjualan/sales_order/create.blade.php
+++ resources/views/superuser/penjualan/sales_order/create.blade.php
@@ -96,33 +96,7 @@
       </div>
 
       <hr />
-        <div class="block">
-          <div class="block-header block-header-default">
-            <h3 class="block-title">Add Product</h3>
-            <a href="#" class="row-add">
-              <button type="button" class="btn bg-gd-sea border-0 text-white">
-                <i class="fa fa-plus mr-10"></i> Row
-              </button>
-            </a>
-          </div>
-          <div class="block-content">
-            <table id="datatable" class="table table-striped table-vcenter">
-              <thead>
-                <tr>
-                  <th class="text-center">Counter</th>
-                  <th class="text-center">Select SKU</th>
-                  <th class="text-center">Product</th>
-                  <th class="text-center">Quantity</th>
-                  <th class="text-center">Price</th>
-                  <th class="text-center">Total</th>
-                  <th class="text-center">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        
       <hr />
       
       <div class="row pt-30 mb-15">
@@ -157,72 +131,7 @@
 
     $('.js-select2').select2();
 
-    var table = $('#datatable').DataTable({
-        paging: false,
-        bInfo : false,
-        searching: false,
-        columns: [
-          {name: 'counter', "visible": false},
-          {name: 'sku', orderable: false, width: "25%"},
-          {name: 'name', orderable: false, searcable: false},
-          {name: 'quantity', orderable: false, searcable: false, width: "5%"},
-          {name: 'price', orderable: false, searcable: false},
-          {name: 'total', orderable: false, searcable: false},
-          {name: 'action', orderable: false, searcable: false, width: "5%"}
-        ],
-        'order' : [[0,'desc']]
-    })
-
-    var counter = 1;
-
-    $('a.row-add').on( 'click', function (e) {
-      e.preventDefault();
-      
-      table.row.add([
-                    counter,
-                    '<select class="js-select2 form-control js-ajax" id="sku['+counter+']" name="sku[]" data-placeholder="Select SKU" style="width:100%" required></select>',
-                    '<span class="name"></span>',
-                    '<input type="number" class="form-control" name="quantity[]" readonly required>',
-                    '<input type="number" class="form-control" name="price[]" readonly required>',
-                    '<input type="number" class="form-control" name="total[]" readonly>',
-                    '<a href="#" class="row-delete"><button type="button" class="btn btn-sm btn-circle btn-alt-danger" title="Delete"><i class="fa fa-trash"></i></button></a>'
-                  ]).draw( false );
-                  // $('.js-select2').select2()
-                  initailizeSelect2();
-      counter++;
-    });
-
-    function initailizeSelect2(){
-      $(".js-ajax").select2({
-        ajax: {
-          url: '',
-          dataType: 'json',
-          delay: 250,
-          data: function (params) {
-            return {
-              q: params.term,
-              _token: "{{csrf_token()}}"
-            };
-          },
-          cache: true
-        },
-        minimumInputLength: 3,
-      });
-
-      $('.js-ajax').on('select2:select', function (e) {
-        var name = e.params.data.name;
-        $(this).parents('tr').find('.name').text(name);
-        $(this).parents('tr').find('input[name="quantity[]"]').removeAttr('readonly');
-        $(this).parents('tr').find('input[name="price[]"]').removeAttr('readonly');
-      });
-
-    };
-
-    $('#datatable tbody').on( 'click', '.row-delete', function (e) {
-      e.preventDefault();
-      
-      table.row( $(this).parents('tr') ).remove().draw();
-    });
+    
 
     $(document).on('click','.select-checkbox',function(){
       if($(this).is(':checked')){
