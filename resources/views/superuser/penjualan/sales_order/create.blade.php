9693492a442c90f31872942ddd23d7099748d5bc
Denki05 <denki.kiki05@gmail.com>
2023-02-24 16:13:17 +0700

    rev view SO Lanjutan

--- resources/views/superuser/penjualan/sales_order/create.blade.php
+++ resources/views/superuser/penjualan/sales_order/create.blade.php
@@ -74,7 +74,7 @@
                     <div class="form-group row">
                       <label class="col-md-4 col-form-label text-right">Kurs</label>
                       <div class="col-md-6">
-                        <input type="number" name="idr_rate" class="form-control" value="0">
+                        <input type="number" name="idr_rate" class="form-control formatRupiah" value="1">
                       </div>
                     </div>
                     @endif
@@ -477,6 +477,29 @@
         clearTimeout(timer)
         timer = setTimeout(fn.bind(this, ...args), ms || 0)
       }
+  }
+
+  function formatRupiah(angka, prefix){
+    angka = angka.toString();
+    var number_string = angka.replace(/[^,\d]/g, '').toString(),
+    split       = number_string.split(','),
+    sisa        = split[0].length % 3,
+    rupiah        = split[0].substr(0, sisa),
+    ribuan        = split[0].substr(sisa).match(/\d{3}/gi);
+   
+    // tambahkan titik jika yang di input sudah menjadi angka ribuan
+    if(ribuan){
+      separator = sisa ? '.' : '';
+      rupiah += separator + ribuan.join('.');
     }
+   
+    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+  };
+
+  $(document).on('keyup','.formatRupiah',function(){
+      let val = $(this).val();
+      $(this).val(formatRupiah(val));
+  })
 </script>
 @endpush
--- resources/views/superuser/penjualan/sales_order/create_lanjutan.blade.php
+++ resources/views/superuser/penjualan/sales_order/create_lanjutan.blade.php
@@ -99,7 +99,12 @@
                   <div class="form-group row">
                     <label class="col-md-4 col-form-label text-right" style="font-size: 10pt;">Kurs<span class="text-danger">*</span></label>
                     <div class="col-5">
-                    <input type="text" name="idr_rate" id="idr_rate"  class="form-control" value="{{ $result->idr_rate }}">
+                    @if ( $result->idr_rate > 1 )
+                      <input type="text" name="idr_rate" id="idr_rate"  class="form-control" value="{{ $result->idr_rate }}" readonly>
+                    @else
+                      <input type="text" name="idr_rate" id="idr_rate"  class="form-control" value="1">
+                    @endif
+                    <!-- <input type="text" name="idr_rate" id="idr_rate"  class="form-control" value="{{ $result->idr_rate }}"> -->
                     </div>
                   </div>
                   @endif
@@ -234,16 +239,16 @@
             <table id="datatable" class="table table-striped">
               <thead>
                 <tr>
-                  <th class="text-center">#</th>
-                  <th class="text-center">Code</th>
-                  <th class="text-center">Product</th>
-                  <th class="text-center">Acuan(USD)</th>
-                  <th class="text-center">Quantity</th>
-                  <th class="text-center">In Stock</th>
-                  <th class="text-center">Packaging</th>
-                  <th class="text-center">Harga</th>
-                  <th class="text-center">Disc</th>
-                  <th class="text-center">jumlah</th>
+                  <th class="text-center" width="2%">#</th>
+                  <th class="text-center" width="10%">Code</th>
+                  <th class="text-center" width="15%">Product</th>
+                  <th class="text-center" width="2%">Acuan(USD)</th>
+                  <th class="text-center" width="5%">Quantity</th>
+                  <th class="text-center" width="5%">In Stock</th>
+                  <th class="text-center" width="10%">Packaging</th>
+                  <th class="text-center" width="13%">Harga</th>
+                  <th class="text-center" width="5%">Disc</th>
+                  <th class="text-center" width="13%">jumlah</th>
                 </tr>
               </thead>
               <tbody>
@@ -251,14 +256,14 @@
                   <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td><select class="js-select2 form-control js-ajax" id="sku[{{ $loop->iteration }}]" name="sku[]" data-placeholder="Select SKU" style="width:100%" required><option value="{{ $detail->product_id }}">{{ $detail->product->code }}</option></select></td>
-                    <td><span class="name">{{ $detail->product->name }}</span></td>
-                    <td><input type="number" class="form-control price" name="price" value="{{ $detail->product->selling_price }}" readonly></td>
-                    <td><input type="number" class="form-control qty" name="qty" value="{{ $detail->qty }}" readonly></td>
-                    <td><input type="number" class="form-control in_stock" name="in_stock"  required></td>
+                    <td><span class="name">{{ $detail->product->name }} - <strong>{{ $detail->product->category->name }}</strong></span></td>
+                    <td><input type="number" class="form-control price" id="price" style="text-align: center;" name="price" value="{{ $detail->product->selling_price }}" readonly></td>
+                    <td><input type="number" class="form-control qty" id="qty" style="text-align: center;" name="qty" value="{{ $detail->qty }}" readonly></td>
+                    <td><input type="number" class="form-control in_stock" id="in_stock" style="text-align: center;" name="in_stock"  required></td>
                     <td><span class="packaging">{{ $detail->packaging_txt()->scalar }}</span></td>
-                    <td><input type="number" class="form-control harga" name="harga" readonly value="{{ $detail->harga }}"></td>
+                    <td><input type="number" class="form-control harga" id="harga" style="text-align: right;" name="harga" readonly value="{{ $detail->harga }}"></td>
                     <td><input type="number" class="form-control disc-cash" name="disc-cash" required></td>
-                    <td><input type="number" class="form-control netto" name="netto" readonly value="{{ $detail->total }}"></td>
+                    <td><input type="number" class="form-control netto" style="text-align: right;" name="netto" readonly value="{{ $detail->total }}"></td>
                   </tr>
                 @endforeach
               </tbody>
@@ -266,7 +271,7 @@
                   <tr class="row-footer-subtotal">
                     <td colspan="9" class="text-right"><span><b>Subtotal</b></span></td>
                     <td class="text-right">
-                      <strong><span class="invoice-subtotal-label"></span></strong>
+                      <input class="form-control sub_total_label" style="text-align: right;" id="sub_total_label" readonly>
                     </td>
                   </tr>
               </tfoot>
@@ -296,6 +301,29 @@
         ] 
     });
 
+    function formatRupiah(angka, prefix){
+      angka = angka.toString();
+      var number_string = angka.replace(/[^,\d]/g, '').toString(),
+      split       = number_string.split(','),
+      sisa        = split[0].length % 3,
+      rupiah        = split[0].substr(0, sisa),
+      ribuan        = split[0].substr(sisa).match(/\d{3}/gi);
+    
+      // tambahkan titik jika yang di input sudah menjadi angka ribuan
+      if(ribuan){
+        separator = sisa ? '.' : '';
+        rupiah += separator + ribuan.join('.');
+      }
+    
+      rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+      return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    };
+
+    $(document).on('keyup','.formatRupiah',function(){
+      let val = $(this).val();
+      $(this).val(formatRupiah(val));
+    })
+
     $('.js-select2').select2();
 
     $(document.body).on('change',".base_disc",function (e) {
@@ -305,30 +333,41 @@
     });
 
     $('#datatable tbody').on( 'keyup', 'input[name="in_stock"]', function (e) {
-      var price = $(this).parents('tr').find('input[name="price"]').val();
-      var disc = $(this).parents('tr').find('input[name="disc-cash"]').val();
-      // var totalBeforeDisc = $(this).val() * price;
-      let totalBeforeDisc = 0;
-      let totalAfterDisc = 0;
-      totalBeforeDisc = $(this).val() * price;
+    var price = $(this).parents('tr').find('input[name="price"]').val();
+    var disc = $(this).parents('tr').find('input[name="disc-cash"]').val();
+    let totalBeforeDisc = 0;
+    let totalAfterDisc = 0;
+    totalBeforeDisc = $(this).val() * price;
 
-      if($('#idr_rate').val() > 1){
-        totalBeforeDisc = parseFloat($('#idr_rate').val()) * parseFloat(totalBeforeDisc);
-        $(this).parents('tr').find('input[name="harga"]').val(totalBeforeDisc);
-        $(this).parents('tr').find('input[name="harga"]').change();
+    if($('#idr_rate').val() > 1){
+      totalBeforeDisc = parseFloat($('#idr_rate').val()) * parseFloat(totalBeforeDisc);
+      $(this).parents('tr').find('input[name="harga"]').val(totalBeforeDisc);
+      $(this).parents('tr').find('input[name="harga"]').change();
 
-        totalAfterDisc = parseFloat(totalBeforeDisc) - (parseFloat($('#idr_rate').val())*disc);
-        $(this).parents('tr').find('input[name="netto"]').val(totalAfterDisc);
-        $(this).parents('tr').find('input[name="netto"]').change();
-      }
-      else{
-        $(this).parents('tr').find('input[name="harga"]').val(totalBeforeDisc);
-        $(this).parents('tr').find('input[name="harga"]').change();
+      totalAfterDisc = parseFloat(totalBeforeDisc) - (parseFloat($('#idr_rate').val()) * disc);
+      $(this).parents('tr').find('input[name="netto"]').val(totalAfterDisc);
+      $(this).parents('tr').find('input[name="netto"]').change();
+    }
+    else{
+      $(this).parents('tr').find('input[name="harga"]').val(totalBeforeDisc);
+      $(this).parents('tr').find('input[name="harga"]').change();
 
-        $(this).parents('tr').find('input[name="netto"]').val(totalAfterDisc);
-        $(this).parents('tr').find('input[name="netto"]').change();
-      }
+      $(this).parents('tr').find('input[name="netto"]').val(totalAfterDisc);
+      $(this).parents('tr').find('input[name="netto"]').change();
+    }
+  });
+
+    // SUM subtotal in product list
+    $('#datatable tbody').on( 'change', 'input[name="netto"]', function (e) {
+      var subtotal = 0;
+      $('input[name="netto"]').each(function(){
+        subtotal += Number($(this).val());
+      });
+      $('#sub_total_label').val(subtotal);
     });
+
+    // change disc cash
+    
   })
 </script>
 @endpush
