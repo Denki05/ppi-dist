93797797a28cdcead63958c87aafe7e9716d9374
Denki05 <denki.kiki05@gmail.com>
2023-07-17 09:21:27 +0700

    rev

--- resources/views/superuser/master/customer/index.blade.php
+++ resources/views/superuser/master/customer/index.blade.php
@@ -24,34 +24,19 @@
 
 <div class="block">
   <div class="block-content">
-    <div id="Wrapper1">
-      <table id="Table1" class="table table-striped display compact hover stripe dataTable no-footer" style="width:100%!important">
-        <thead>
-            <tr class="bir-selection-color1 text-center">
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-      </table>
-    </div>
-    <div id="Wrapper2" style="display:none;">
-        <table id="Table2" class="table table-striped display compact hover stripe dataTable no-footer" style="padding-left: 15px!important">
-            <thead>
-                <tr class="bir-selection-color1 text-center">
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-        </table>
-    </div>
+    <table id="store_list" class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th>#</th>
+          <th>Store</th>
+          <th>Category</th>
+          <th>Kota</th>
+          <th>Provinsi</th>
+          <th>Tempo</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
   </div>
 </div>
 @endsection
@@ -72,146 +57,81 @@
 
 @push('scripts')
 <script>
-    // var dataSet1 = [{ "HeaderId": "1", "Group": "Group 1" }
-    //                     ];
-
-    // var dataSet2 = [{ "LineNumber": "1", "HeaderId": "1", "ProductId": "1011" },
-    //                 { "LineNumber": "2", "HeaderId": "1", "ProductId": "1012" }
-    //                 ];
-
-    //dataTables default values
-    var dataTablesdefaultPageLength = 10;
-    var dataTablesPageLengthDisplay = [10, 50, 100, 250, 500];
-
-    var dataTablesserverSide = false;
-    var dataTablesautoWidth = true;
-    var dataTablesresponsive = true;
-    var dataTablesprocessing = true;        // for show progress bar 
-    var dataTablesfilter = false;            // this is for disable filter (search box)
-    var dataTablesorderMulti = false;       // for disable multiple column at once  
-
-    var dataTablesscrollX = false;
-
-    $(document).ready(function () {
-
-        // Get the Detail Table as Template for Every Row
-        detailsTableHtml = $("#Table2").html();
-
-        var table1 = $("#Table1").DataTable({
-            async: true,
-                serverSide: false, // for process server side
-                deferRender: true, // deferred rendering
-                scrollX: dataTablesscrollX,
-                deferLoading: 0, //Disable Initial Load
-                processing: dataTablesprocessing, // for show progress bar
-                filter: dataTablesfilter,
-                orderMulti: true,
-                autoWidth: dataTablesautoWidth,
-                responsive: dataTablesresponsive,
-                pageLength: dataTablesdefaultPageLength,
-                lengthMenu: [dataTablesPageLengthDisplay, dataTablesPageLengthDisplay],
-                ajax : {
-                    "url": '{{route('superuser.master.customer.json')}}',
-                    "dataType": "json",
-                    "type": "GET",
-                    "data":{ _token: "{{csrf_token()}}"}
-                },
-                order: [[1, 'asc']],
-                columnDefs: [],
-                columns: [
-                {
-                    data: null, name: "Group", title: "", className: 'details-control', orderable: false, width: 5,
-                    render: function (data, type, row, meta) {
-                        return '<i class="fa fa-plus"></i>';
-                    }
-                },
-                { data: "store_name", name: "master_customer.name", title: "Store" },
-                { data: "category_name", name: "master_customer_categories.name", title: "Category", orderable: false },
-                { data: "text_kota", name: "master_customer.text_kota", title: "Kota", orderable: false },
-                { data: "text_provinsi", name: "master_customer.text_provinsi", title: "Provinsi", orderable: false },
-                { data: "tempo_limit", name: "master_customer.tempo_limit", title: "Tempo", orderable: false },
-                { data: "action", title: "Action", orderable: false },
-            ]
-            , rowId: 'customer_id'
-        });
-
-        $('#Table1 tbody').on('click', 'td i', function () {
-
-            var tr = $(this).closest('tr');
-            var row = table1.row(tr);
-
-            if (row.child.isShown()) {
-                // This row is already open - close it
-                destroyChild(row);
-                tr.removeClass('shown');
-            }
-            else {
-                // Open this row
-                createChild(row);
-                tr.addClass('shown');
-            }
-
-        });
+    $(document).ready(function() {
+      var table = $('#store_list').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{{ route('superuser.master.customer.json') }}',
+        columns: [
+            {
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": "<i class='fa fa-angle-down' aria-hidden='true'></i>"
+            },
+            {data: 'store_name', name: 'master_customers.name'},
+            {data: 'category_name', name: 'master_customer_categories.name'},
+            {data: 'text_kota', name: 'master_customers.text_kota'},
+            {data: 'text_provinsi', name: 'master_customers.text_provinsi'},
+            {data: 'tempo_limit', name: 'master_customers.tempo_limit'},
+            {data: 'action', orderable: false, searcable: false}
+        ],
+        order: [[1, 'asc']]
     });
 
-    function createChild(row) {
-        // This is the table we'll convert into a DataTable
-        var table2 = $('<table class="display" width="100%"/>');
+    // Add event listener for opening and closing details
+    $('#store_list tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
 
-        // Display it the child row
-        row.child(table2).show();
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    });
 
-        // Initialise as a DataTable
-        var usersTable = table2.DataTable({
-            async: true,
-          serverSide: false, // for process server side
-          scrollX: false, //Enable Horizonal Scroll
-          deferRender: true, // deferred rendering
-          processing: dataTablesprocessing, // for show progress bar
-          filter: false,
-          orderMulti: true,
-          info: false,
-          paging: false,
-          select: false,
-          autoWidth: dataTablesautoWidth,
-          responsive: dataTablesresponsive,
-          ajax : {
-                    "url": '{{route('superuser.master.customer.json')}}',
-                    "dataType": "json",
-                    "type": "GET",
-                    "data":{ _token: "{{csrf_token()}}"}
-          },
-            order: [[0, 'asc']],
-            columnDefs : [
-                        { targets : [3],
-                            render : function (data, type, row) {
-                                switch(data) {
-                                    case 0 : return '<span class="badge badge-danger">No</span>'; 
-                                break;
-                                    case 1 : return '<span class="lbadge badge-success">Yes</span>'; 
-                                break;
-                                }
-                            }
-                        },
-                ],
-            columns: [
-                { data: "member_name", name: "master_customer_other_addresses.name", title: "Member", autoWidth: true },
-                { data: "member_kota", name: "master_customer_other_addresses.text_kota", title: "Kota", autoWidth: true },
-                { data: "member_provinsi", name: "master_customer_other_addresses.text_provinsi", title: "Provinsi", autoWidth: true },
-                { data: "member_default", name: "master_customer_other_addresses.member_default", title: "Default", autoWidth: true },
-                { data: "action_member", title: "Action" }
-                
-            ],
-        });
+    /* Formatting function for row details - modify as you need */
+    function format ( d ) {
+        // `d` is the original data object for the row
+        // return '<table class="table table table-hover">'+
+        //     '<tr>'+
+        //         '<td>Member:</td>'+
+        //         '<td>'+d.member_name+'</td>'+
+        //         '</tr>'+
+        //     '<tr>'+
+        //         '<td>Email:</td>'+
+        //         '<td>'+d.email+'</td>'+
+        //         '</tr>'+
+        //     '<tr>'+
+        //         '<td>Extra info:</td>'+
+        //         '<td>And any further details here (images etc)...</td>'+
+        //         '</tr>'+
+        //     '</table>';
+        return '<table class="table">'+
+                  '<thead>'+
+                      '<tr class="table-active">'+
+                        '<th>Member</th>'+
+                        '<th>Kota</th>'+
+                        '<th>Provinsi</th>'+
+                        '<th>Default</th>'+
+                      '</tr>'+
+                  '</thead>'+
+                  '<tbody>'+
+                      '<tr>'+
+                        '<td>'+d.member_name+'</td>'+
+                        '<td>'+d.member_kota+'</td>'+
+                        '<td>'+d.member_provinsi+'</td>'+
+                        '<td>'+d.member_default+'</td>'+
+                      '</tr>'+
+                  '</tbody>'
+                '</table>';
     }
-
-    function destroyChild(row) {
-        var table2 = $("table", row.child());
-        table2.detach();
-        table2.DataTable().destroy();
-
-        // And then hide the row
-        row.child.hide();
-    }
+    })
 </script>
 @endpush
