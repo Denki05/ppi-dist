ca18c49e3c37d36a7259fb64c343df22cb7186ca
Denki05 <denki.kiki05@gmail.com>
2023-08-31 08:51:47 +0700

    rev

--- resources/views/superuser/master/customer_other_address/edit.blade.php
+++ resources/views/superuser/master/customer_other_address/edit.blade.php
@@ -51,7 +51,7 @@
                 <div class="form-group row">
                   <label for="name" class="col-sm-2 col-form-label">Category<span class="text-danger">*</span></label>
                   <div class="col-sm-10">
-                    <input type="text" id="category" name="category" placeholder="Category Member" class="form-control" value="{{$other_address->customer->category->name}}" readonly>
+                    <input type="text" id="category" name="category" placeholder="Category Member" class="form-control" value="{{$other_address->store->category->name}}" readonly>
                   </div>
                 </div>
               </div>
